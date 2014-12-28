a2869c46f8753f25ebfa2e01a173d78c8658e885
Luis Fernando Montoya <luismec90@gmail.com>
2014-12-28 14:58:30 -0500

    Mejoras

--- app/models/Achievement.php
+++ app/models/Achievement.php
@@ -2,4 +2,9 @@
 
 class Achievement extends \Eloquent {
 	protected $fillable = [];
+
+	public function imagePath()
+	{
+		return asset('assets/images/course/achievements/' . $this->id.'.png');
+	}
 }
