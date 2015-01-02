fdbf5c0b02b1706b108b96607c777a6d22a0e2a1
Luis Fernando Montoya <luismec90@gmail.com>
2015-01-02 18:23:32 -0500

    Mejoras

---
+++ app/database/migrations/2015_01_02_190730_create_approved_quizzes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateApprovedQuizzesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('approved_quizzes', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->integer('quiz_id')->unsigned()->index();
+            $table->foreign('quiz_id')->references('id')->on('quizzes')->onDelete('cascade');
+            $table->integer('score');
+            $table->decimal('best_time',8,3)->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('approved_quizzes');
+    }
+
+}
---
+++ app/models/ApprovedQuiz.php
@@ -0,0 +1,5 @@
+<?php
+
+class ApprovedQuiz extends \Eloquent {
+	protected $fillable = [];
+}
