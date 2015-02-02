60efc4c1fdadd2987ef3c0a4fc304f83558485a3
Luis Fernando Montoya <luismec90@gmail.com>
2015-02-02 18:34:44 -0500

    Mejoras

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -15,13 +15,13 @@ class PagesController extends BaseController {
     {
         ini_set('max_execution_time', 300);
 
-        $students = [['MATEMÁTICAS BÁSICAS','1000001','1','CLASE TEORICA','MA-JU','06:00-08:00','David Santiago Correa C.','dscorreac@unal.edu.co','1.128.395.497'],['MATEMÁTICAS BÁSICAS','1000001','2','CLASE TEORICA','MI-VI','12:00-14:00','Jhon Edison Hinestroza','jhehinestrozara@unal.edu.co','98.552.696'],['MATEMÁTICAS BÁSICAS','1000001','4','CLASE TEORICA','MA-JU','10:00-12:00','EDGAR ARTURO RAMOS NAVARRETE','earamosn@unal.edu.co','79.292.147'],['MATEMÁTICAS BÁSICAS','1000001','6','CLASE TEORICA','MI-VI','10:00-12:00','William Mateus','wimateusav@unal.edu.co','1.110.480.931'],['MATEMÁTICAS BÁSICAS','1000001','7','CLASE TEORICA','MI-VI','08:00-10:00','Fernando Puerta Ortiz','fpuerta@unal.edu.co','19.076.825'],['MATEMÁTICAS BÁSICAS','1000001','8','CLASE TEORICA','MI-VI','06:00-08:00','Jhon Fredy Ruiz','jfruiz@unal.edu.co','71.370.949'],['MATEMÁTICAS BÁSICAS','1000001','9','CLASE TEORICA','MI-VI','14:00-16:00','Sigifredo De Jesus Herron Osorio','sherron@unal.edu.co','6.984.720'],['MATEMÁTICAS BÁSICAS','1000001','10','CLASE TEORICA','MA-JU','16:00-18:00','Sergio Andrés Narváez J.','sanarvaez@unal.edu.co','1.036.396.939'],['MATEMÁTICAS BÁSICAS','1000001','11','CLASE TEORICA','MI-VI','16:00-18:00','Javier Martinez Gonzalez','javmartinezgon@unal.edu.co','1.102.837.116'],['MATEMÁTICAS BÁSICAS','1000001','12','CLASE TEORICA','MA-JU','16:00-18:00','John Jader Mira Albanes','jjmira@unal.edu.co','8.013.110'],['MATEMÁTICAS BÁSICAS','1000001','13','CLASE TEORICA','MA-JU','18:00-20:00','Jose Gabriel Hernández Montiel','jghernandezm@unal.edu.co','1.064.985.348'],['MATEMÁTICAS BÁSICAS','1000001','14','CLASE TEORICA','MA-JU','12:00-14:00','EDGAR ARTURO RAMOS NAVARRETE','earamosn@unal.edu.co','79.292.147'],['MATEMÁTICAS BÁSICAS','1000001','15','CLASE TEORICA','MA-JU','14:00-16:00','Julio Cesar Morales Cuervo','jcmorale@unal.edu.co','3.454.766']];
+        $students = [['1017214613','Daniela Arango Ospina','darangoo@unal.edu.co'],[' 1036952857','Daniel Escudero Ospina','deescuderoo@unal.edu.co'],[' 1037629252','Manuel Alberto Ayaso','maayasob@unal.edu.co']];
 
 
         foreach ($students as $index => $row)
         {
-            $row[8]=str_replace(".","",$row[8]);
-            $student = User::where('dni', $row[8])->first();
+
+            $student = User::where('dni', $row[0])->first();
 
             if (is_null($student))
             {
@@ -44,9 +44,9 @@ class PagesController extends BaseController {
         */
                 $student = new User;
 
-                $student->dni = $row[8];
-                $student->first_name =$row[6];
-                $student->email = $row[7];
+                $student->dni = $row[0];
+                $student->first_name =$row[1];
+                $student->email = $row[2];
                 $student->avatar = 'default.png';
                 $student->password = Hash::make($student->dni);
                 $student->confirmed = 1;
