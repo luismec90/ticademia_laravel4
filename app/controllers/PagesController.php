42d1eed6da0b356b9c8a9925ad376c2f6fff9b38
Luis Fernando Montoya <luismec90@gmail.com>
2015-02-03 16:56:21 -0500

    Mejoras

--- app/controllers/PagesController.php
+++ app/controllers/PagesController.php
@@ -7,14 +7,27 @@ class PagesController extends BaseController {
     public function test()
     {
 
+        $course = Course::first();
+
+        $students = $course->users()->where('role', 1)
+            ->where('user_id', '=', 1)
+            ->get()->lists('email');
+
+        array_push($students, 'luismec90@gmail.com');
+        array_push($students, 'lfmontoyag@unal.edu.co');
+
+
+
         //return View::make('emails.auth.hi', compact('courses'));
 
-        Mail::send('emails.auth.hi', [], function ($message)
+        Mail::send('emails.auth.hi', [], function ($message) use ($students)
         {
-            $message->to('lfmontoyag@unal.edu.co')
-                ->cc('luismec90@gmail.com')
+            $message->to('soporte.ticademia@gmail.com')
+                ->bcc($students)
                 ->subject('Bienvenido!');
         });
+
+        return View::make('emails.auth.hi', compact('courses'));
 
     }
 
@@ -22,7 +35,7 @@ class PagesController extends BaseController {
     {
         ini_set('max_execution_time', 300);
 
-        $students = [['3438502','Cabarcas Jaramillo Daniel','dcabarc@unal.edu.co']];
+        $students = [['3438502', 'Cabarcas Jaramillo Daniel', 'dcabarc@unal.edu.co']];
 
 
         foreach ($students as $index => $row)
@@ -45,14 +58,14 @@ class PagesController extends BaseController {
 
                     $i ++;
                 }
-                $firstName=trim($firstName);
-                $lastName=trim($lastName);
+                $firstName = trim($firstName);
+                $lastName = trim($lastName);
 
                 $student = new User;
 
                 $student->dni = $row[0];
-                $student->first_name =$firstName;
-                $student->last_name =$lastName;
+                $student->first_name = $firstName;
+                $student->last_name = $lastName;
                 $student->email = $row[2];
                 $student->avatar = 'default.png';
                 $student->password = Hash::make($student->dni);
