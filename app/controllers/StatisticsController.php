e6a87a4ad6a394de3149d99da9329a35e82b459a
Luis Fernando Montoya <luismec90@gmail.com>
2015-02-16 10:03:11 -0500

    Mejoras

--- app/controllers/StatisticsController.php
+++ app/controllers/StatisticsController.php
@@ -462,6 +462,7 @@ class StatisticsController extends \BaseController {
             });
         })->get();
 
+        return $course;
 
         $date = $course->start_date;
         $end_date = date('Y-m-d');
@@ -469,7 +470,7 @@ class StatisticsController extends \BaseController {
         $months = ['', '01' => 'Ene', '02' => 'Feb', '03' => 'Mar', '04' => 'Abr', '05' => 'May', '06' => 'Jun',
                        '07' => 'Jul', '08' => 'Ago', '09' => 'Sep', '10' => 'Oct', '11' => 'Nov', '12' => 'Dic'];
 
-        return $course;
+
 
         while (strtotime($date) <= strtotime($end_date))
         {
