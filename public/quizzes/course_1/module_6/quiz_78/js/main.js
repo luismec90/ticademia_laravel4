0f1758b42f8c7ae9fe662cb1c14951e52ea0c64f
Luis Fernando Montoya <luismec90@gmail.com>
2015-02-15 20:36:05 -0500

    Mejoras

---
+++ public/quizzes/course_1/module_6/quiz_78/js/main.js
@@ -0,0 +1,84 @@
+var m,k,j;
+
+$(function() {
+	try{
+		API = getAPI();
+		API.LMSInitialize("");
+	}catch(e){
+		console.log(e);
+	}
+
+    m = getRandom(3,21);
+    k = getRandom(2,21);
+    j = getRandom(3,21);
+
+    var correctAnswer1 = 2*k;
+    var correctAnswer2 = 2*k+1;
+    //var missConception1 = n;
+    //console.log(correctAnswer1 + " " + correctAnswer2);
+    draw();
+
+    $("#verificar").click(function() {
+        var valor1 = $("#answer1").val().trim();
+        var valor2 = $("#answer2").val().trim();
+        if (valor1 != "" && valor2 != "") {
+            $("#correcto").addClass("hide");
+            $("#feedback").addClass("hide");
+            var calificacion = 0;
+            var feedback = "";
+            valor1 = parseFloat(valor1);
+            valor2 = parseFloat(valor2);
+			
+			if(valor1 == correctAnswer1 && valor2 == correctAnswer2){
+				calificacion = 1.0;
+				$("#correcto").html("Calificación: <b>" + calificacion + "</b>").removeClass("hide");
+            }else{
+				calificacion = 0.0;
+				$("#feedback").html("Calificación: <b>" + calificacion + "</b> <br> ...").removeClass("hide");
+            }
+            $(this).attr("disabled", true);
+            API.closeQuestion();
+            if (typeof API.calificar == 'function') {
+                API.calificar(calificacion, feedback);
+            }
+            API.LMSSetValue("cmi.core.score.raw", calificacion);
+            API.LMSFinish("feedback", feedback);
+            API.notifyDaemon(calificacion);
+        }
+    });
+    $("#aceptar").click(function() {
+        window.parent.location.reload();
+    });
+    $('#modal').on('hide.bs.modal', function(e) {
+        window.parent.location.reload();
+    });
+
+});
+function getRandom(bottom, top) {
+    return Math.floor(Math.random() * (1 + top - bottom)) + bottom;
+}
+function getRandomFrom(vals){
+	return vals[getRandom(0,vals.length-1)];
+}
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+function decimalComparison(v1,v2,d){
+    d = Math.pow(10,d);
+    v1 = Math.round(v1*d);
+    v2 = Math.round(v2*d);
+    return v1==v2;
+}
+function draw(){
+    $('.mvar[value=e1]').html(m);
+    $('.mvar[value=e2]').html(-k*(2-j));
+    $('.mvar[value=e3]').html(k);
+    $('.mvar[value=e4]').html(j);
+    $('.mvar[value=e5]').html(m-2);
+}
---
+++ public/quizzes/course_1/module_6/quiz_81/js/main.js
@@ -0,0 +1,108 @@
+var a,b,c,m;
+
+$(function() {
+	try{
+		API = getAPI();
+		API.LMSInitialize("");
+	}catch(e){
+		console.log(e);
+	}
+
+    a = getRandom(2,10)*getRandomFrom([-1,1]);
+    b = getRandom(2,10)*getRandomFrom([-1,1]);
+    c = getRandom(2,10)*getRandomFrom([-1,1]);
+    m = getRandom(3,10);
+
+    //console.log(correctAnswer + " " + missConception1);
+    var correctAnswer = draw();
+
+    $("#verificar").click(function() {
+        var valor = $("input[name=answer]:checked").val().trim();
+        if (valor != "") {
+            $("#correcto").addClass("hide");
+            $("#feedback").addClass("hide");
+            var calificacion = 0;
+            var feedback = "";
+            valor = parseFloat(valor);
+            switch (valor) {
+                case correctAnswer:
+                    calificacion = 1.0;
+                    $("#correcto").html("Calificación: <b>" + calificacion + "</b>").removeClass("hide");
+                    break;
+                default:
+                    calificacion = 0.0;
+                    $("#feedback").html("Calificación: <b>" + calificacion + "</b> <br> ...").removeClass("hide");
+                    break;
+            }
+            $(this).attr("disabled", true);
+            API.closeQuestion();
+            if (typeof API.calificar == 'function') {
+                API.calificar(calificacion, feedback);
+            }
+            API.LMSSetValue("cmi.core.score.raw", calificacion);
+            API.LMSFinish("feedback", feedback);
+            API.notifyDaemon(calificacion);
+        }
+    });
+    $("#aceptar").click(function() {
+        window.parent.location.reload();
+    });
+    $('#modal').on('hide.bs.modal', function(e) {
+        window.parent.location.reload();
+    });
+
+});
+function getRandom(bottom, top) {
+    return Math.floor(Math.random() * (1 + top - bottom)) + bottom;
+}
+function getRandomFrom(vals){
+	return vals[getRandom(0,vals.length-1)];
+}
+function draw(){
+	var correct = 0;
+    var d = b*b-4*a*(c-m/(m+1));
+    var c1,c2,c3;
+    if(d>0){
+        c1='2';
+        c2='1';
+        c3='0';
+    }else if(d<0){
+        c1='0';
+        c2='1';
+        c3='2';
+    }else{
+        c1='1';
+        c2='0';
+        c3='2';
+    }
+	var answers = [c1,
+                    c2,
+                    c3,
+                    '3',
+                    '4'];
+	var is = [0,1,2,3,4];
+	shuffleArray(is);
+	var i = 0;
+	while(i<5){
+		$("#label"+(i+1)).html(answers[is[i]]);
+		if(is[i]==0)correct=i+1;
+		i++;
+	}
+	
+    $('.mvar[value=e1]').html(a);
+    $('.mvar[value=e2]').html(b);
+    $('.mvar[value=e3]').html(c);
+    $('.mvar[value=e4]').html(m);
+    $('.mvar[value=e5]').html(m+1);
+	return correct;
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
